43dc931643873dab819764cd776f692540d3d09c
Vicente Piltcher <vipiltcher62@gmail.com>
2025-03-10 17:56:46 -0300

    topico1

--- App.java
+++ App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        esportivo.abastece(TipoCombustivel.GASOLINA, 30);
+        esportivo.viaja(3);
+        System.out.println(esportivo);
 
 
 
